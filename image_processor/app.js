16d6f9d336eb47f38c7ed8bc361773c35aa54613
Bilal Ahmed <bilal@myadbox.com.au>
2018-10-16 09:54:20 +1100

    console.log for exception

--- image_processor/app.js
+++ image_processor/app.js
@@ -191,8 +191,12 @@ exports.helpers = {
     }
   },
   cleanTemp: function(dirPath) {
-    try { var files = fs.readdirSync(dirPath); }
-    catch(e) { return; }
+    try {
+      var files = fs.readdirSync(dirPath);
+    } catch(e) {
+      console.log(e);
+      return;
+    }
     if (files.length > 0) {
       for (var i = 0; i < files.length; i++) {
         var filePath = dirPath + '/' + files[i];
